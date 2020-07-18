a1593ba08a4655ece0ea8b4c5991463d6ee75238
Genetical <18619788+Genetical@users.noreply.github.com>
2020-07-18 11:06:21 +0100

    Make interface more robust
    Will now check a call map, if not found, it will fallback to pinging the dll directly. Also limits pointless wrapper functions

--- F4MP/Librg/interface.py
+++ F4MP/Librg/interface.py
@@ -6,28 +6,40 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 dll = ctypes.cdll.LoadLibrary(os.path.join(BASE_DIR, "bin/librg.dll"))
 
 
-def init(ctx):
-    dll.librg_init(ctx)
+class Interface:
+    @staticmethod
+    def network_start(ctx, address: bytes, port: int):
+        address = Address(port, address)
+        return bool(dll.librg_network_start(ctx, address))
 
 
-def is_client(ctx):
-    return bool(dll.librg_is_client(ctx))
+def dll_func(attr, cast=bool):
+    def predicate(*args, **kwargs):
+        return cast(dll.__getattr__(attr)(*args, **kwargs))
+    return predicate
 
 
-def is_connected(ctx):
-    return bool(dll.librg_is_connected(ctx))
+func_map = {
+    "init": ("librg_init",),
+    "is_client": ("librg_is_client",),
+    "is_connected": ("librg_is_connected",),
+    "event_add": ("librg_event_add",),
+    "tick": ("librg_tick",),
+}
+func_map.update(
+    {func: getattr(Interface, func) for func in dir(Interface)
+     if callable(getattr(Interface, func)) and not func.startswith("__")})
+
+__all__ = tuple(func_map.keys())
 
 
-def event_add(ctx, event_id, callback):
-    if not dll.librg_event_add(ctx, event_id, callback):
-        raise
-
-
-def network_start(ctx, address: bytes, port: int):
-    address = Address(port, address)
-
-    ret = dll.librg_network_start(ctx, address)
-
-
-def tick(ctx):
-    dll.librg_tick(ctx)
+def __getattr__(item):
+    try:
+        res = func_map[item]
+    except KeyError:
+        return dll.__getattr__(item)
+    else:
+        if callable(res):
+            return res
+        else:
+            return dll_func(*res)
